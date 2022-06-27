0b43adf24a6d11e273f774609ee0acc7e4e98936
Sunil <sunilkumarpatra@hotmail.com>
2022-06-27 15:07:26 +0530

    index

---
+++ includes/menubar.ini.php
@@ -0,0 +1,77 @@
+<div class="sb-top-bar">
+          <a href="home-1.html" class="sb-logo-frame">
+            <!-- logo img -->
+            <img src="img/ui/logo.svg" alt="Starbelly">
+          </a>
+          <!-- menu -->
+          <div class="sb-right-side">
+            <nav id="sb-dynamic-menu" class="sb-menu-transition">
+              <ul class="sb-navigation">
+                <li class="sb-active sb-has-children">
+                  <a href="home-1.html">Home</a>
+                 
+                </li>
+                <li class="sb-has-children">
+                  <a href="about-2.html">Pages</a>
+                  <ul>
+                    <li><a href="about-1.html">About 1</a></li>
+                    <li><a href="about-2.html">About 2</a></li>
+                    <li><a href="blog-1.html">Blog style 1</a></li>
+                    <li><a href="blog-2.html">Blog style 2</a></li>
+                    <li><a href="blog-3.html">Blog style 3</a></li>
+                    <li><a href="publication-1.html">Publication 1</a></li>
+                    <li><a href="publication-2.html">Publication 2</a></li>
+                    <li><a href="publication-3.html">Publication 3</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="gallery-2.html">Gallery 2</a></li>
+                    <li><a href="reviews.html">Reviews</a></li>
+                    <li><a href="reservation.html">Reservation</a></li>
+                    <li><a href="faq.html">FAQ</a></li>
+                    <li><a href="404.html">404</a></li>
+                  </ul>
+                </li>
+                <li class="sb-has-children">
+                  <a href="menu-1.html">Menu</a>
+                  <ul>
+                    <li><a href="menu-1.html">Menu style 1</a></li>
+                    <li><a href="menu-2.html">Menu style 2</a></li>
+                    <li><a href="menu-3.html">Menu style 3</a></li>
+                    <li><a href="menu-4.html">Menu style 4</a></li>
+                    <li><a href="menu-5.html">Menu style 5</a></li>
+                    <li><a href="menu-6.html">Menu style 6</a></li>
+                  </ul>
+                </li>
+                <li class="sb-has-children">
+                  <a href="shop-1.html">Shop</a>
+                  <ul>
+                    <li><a href="shop-1.html">Shop style 1</a></li>
+                    <li><a href="shop-2.html">Shop style 2</a></li>
+                    <li><a href="shop-list-1.html">Shop list 1</a></li>
+                    <li><a href="shop-list-2.html">Shop list 2</a></li>
+                    <li><a href="product.html">Product page</a></li>
+                    <li><a href="cart.html">Cart</a></li>
+                    <li><a href="checkout.html">Checkout</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="contact.html">Contact</a>
+                </li>
+              </ul>
+            </nav>
+            <div class="sb-buttons-frame">
+              <!-- button -->
+              <div class="sb-btn sb-btn-2 sb-btn-gray sb-btn-icon sb-m-0 sb-btn-cart">
+                <span class="sb-icon">
+                  <img src="img/ui/icons/cart.svg" alt="icon">
+                </span>
+                <i class="sb-cart-number">5</i>
+              </div>
+              <!-- button end -->
+              <!-- menu btn -->
+              <div class="sb-menu-btn"><span></span></div>
+              <!-- info btn -->
+              <div class="sb-info-btn"><span></span></div>
+            </div>
+          </div>
+        </div>
+      </div>
